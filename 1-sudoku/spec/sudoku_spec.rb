ba5b1cef0a343651ae7d90ebcd16bb6843d0ca33
Sean Arnold <me@seanarnold.co.nz>
2015-05-20 21:47:09 +1200

    Add Sudoku specs

---
+++ 1-sudoku/spec/sudoku_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Sudoku do
+
+  describe '#row_for_cell' do
+    it 'should return the row of the given cell';
+  end
+
+  describe '#column_for_cell' do
+    it 'should return the column of the given cell';
+  end
+
+  describe '#square_for_cell' do
+    it 'should return the cells in the square for the given cell';
+  end
+
+  describe '#valid_game?' do
+    it 'should determine if the sudoku board is valid or not';
+  end
+
+end
