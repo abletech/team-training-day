f8296653b7ba207a2b775aa8df82f1f37c25ae32
Sean Arnold <me@seanarnold.co.nz>
2015-06-26 11:34:04 +1200

    1st attempt

--- 1-sudoku/spec/sudoku_spec.rb
+++ 1-sudoku/spec/sudoku_spec.rb
@@ -2,16 +2,35 @@ require 'spec_helper'
 
 RSpec.describe Sudoku do
 
+  sudoku = Sudoku.new("609238745274561398853947621486352179792614583531879264945723816328196457167485932")
+
   describe '#row_for_cell' do
-    it 'should return the row of the given cell';
+    let(:cell) { Cell.new(x:0, y:0) }
+
+    subject { sudoku.row_for_cell(cell) }
+
+    it 'should return the row of the given cell' do
+      expect(subject).to eq ["6", "0", "9", "2", "3", "8", "7", "4", "5"]
+    end
   end
 
   describe '#column_for_cell' do
-    it 'should return the column of the given cell';
+    let(:cell) { Cell.new(x:3, y:5) }
+
+    subject { sudoku.column_for_cell(cell) }
+
+    it 'should return the column of the given cell' do
+      expect(subject).to eq ["2", "5", "9", "3", "6", "8", "7", "1", "4"]
+    end
   end
 
   describe '#square_for_cell' do
-    it 'should return the cells in the square for the given cell';
+    let(:cell) { Cell.new(x:7, y:7) }
+
+    subject { sudoku.square_for_cell(cell) }
+    it 'should return the cells in the square for the given cell' do
+      expect(subject).to eq ["8", "1", "6", "4", "5", "7", "9", "3", "2"]
+    end
   end
 
   describe '#valid_game?' do
--- 1-sudoku/sudoku.rb
+++ 1-sudoku/sudoku.rb
@@ -1,10 +1,72 @@
 class Sudoku
+  attr_reader :board, :starting_board
   def initialize(board_string)
+    # create the board arrays
+    @starting_board = board_string.chars.each_slice(9).to_a
+    @board = @starting_board.clone
+    # set solved state
   end
 
   def solve!
+
+    starting_board.each_with_index do |row, y|
+      row.each_with_index do |c, x|
+        if c == '0'
+          cell = Cell.new(y: y, x: x)
+          unless c = possible_numbers_for_cell(cell).sample
+            raise "Error"
+          end
+        end
+        board[y][x] = c
+      end
+    end
+    print_board
+    # iterate around board
+    # when we find a '0', choose from the possible numbers for cell
+    # if no numbers, we failed.
   end
 
-  def board
+  def print_board
+
+    board.each_with_index do | row, index |
+      if index % 3 == 0
+        puts "----------------------------------------"
+      end
+
+      puts row.to_s
+    end
+  end
+
+  def possible_numbers_for_cell(cell)
+    (1..9).to_a - row_for_cell(cell) - column_for_cell(cell) - square_for_cell(cell)
+  end
+
+  def row_for_cell(cell)
+    board[cell.y]
+  end
+
+  def column_for_cell(cell)
+    board.transpose[cell.x]
+  end
+
+  def square_for_cell(cell)
+    square_y = (cell.y / 3) * 3
+    square_x = (cell.x / 3)
+
+    square = []
+    (0..2).each do |i|
+      square << board[square_y+i].each_slice(3).to_a[square_x]
+    end
+
+    square.flatten
+  end
+
+end
+
+class Cell
+  attr_reader :x, :y
+  def initialize(y:, x:)
+    @y = y
+    @x = x
   end
 end
