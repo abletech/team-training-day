a78b14e485320ca652ed52856b30ff4cb909ae53
Naiki Pohe <naiki.pohe@abletech.co.nz>
2015-06-26 12:22:04 +1200

    hipster

---
+++ 2-javascript_racer/actual_solution/coffee_run.js
@@ -0,0 +1,14 @@
+var countdown = function(){
+  $('.player img').css("left","0px")
+  start();
+};
+
+var start = function(){
+  $(document).on('keyup', function(event){
+
+  });
+};
+
+$(document).ready(function(){
+  countdown();
+});
